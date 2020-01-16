c115cac911c2bdc596d7b7c9f5ba3d30247cb2e8
Frank Mutter <frank@mutter.email>
2020-01-16 22:02:39 +0100

    added python database, untested

--- bot/mensa.py
+++ bot/mensa.py
@@ -1,12 +1,15 @@
 from urllib.request import urlopen
 import re
+import os
+import sqlite3
 
-mensas = ['Mensa Rempartstraße', 'Mensa Institutsviertel',
+mensas = ['Mensa Rempartstrasse', 'Mensa Institutsviertel',
           'Mensa Littenweiler', 'Mensa Flugplatz', 'Mensa Furtwangen',
           'Mensa Offenburg', 'Mensa Gengenbach', 'Mensa Kehl',
           'Mensa Schwenningen', 'Mensa Trossingen',
           'Ausgabestelle EH Freiburg', 'MusiKantine', 'OHG Furtwangen']
 
+DB_NAME = "users.db"
 
 def get_data(mensa):
     assert mensa in mensas
@@ -24,4 +27,31 @@ def get_data(mensa):
         menus = re.findall(r'<h4.*?>(.*?)</h4><div.*?>\s*(.*?)<', rest)
         if menus:
             result[day] = [(t, f.strip()) for t, f in menus]
+    return result
+
+
+def initialize_database():
+    if os.path.exists(DB_NAME):
+        return
+    connection = sqlite3.connect(DB_NAME)
+    cursor = connection.cursor()
+    cursor.execute("CREATE TABLE users (user VARCHAR(30), mensa VARCHAR(30));")
+    connection.commit()
+    connection.close()
+
+
+def add_mensa(user, mensa):
+    connection = sqlite3.connect(DB_NAME)
+    cursor = connection.cursor()
+    cursor.execute("INSERT INTO users VALUES (%r, %r);" % (user, mensa))
+    connection.commit()
+    connection.close()
+
+
+def get_mensas(user):
+    connection = sqlite3.connect(DB_NAME)
+    cursor = connection.cursor()
+    cursor.execute("SELECT DISTINCT mensa FROM users WHERE user=%r" % user) 
+    result = cursor.fetchall()
+    connection.close()
     return result
