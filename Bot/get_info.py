ce32d886c16b233d4694707888df7ac6c560ae8a
schuessf <fr.schuessele@gmx.de>
2020-02-17 00:37:24 +0100

    Refactor get_info

--- Bot/get_info.py
+++ Bot/get_info.py
@@ -1,9 +1,10 @@
-from db_tools import get_all_user_and_mensas
+from db_tools import get_all_user_and_mensas, get_all_mensa_subscriptions
 
 users_mensas = get_all_user_and_mensas()
 print("unique sending messages %d" % len(users_mensas))
+print("unique users %d" % len(set(i[0] for i in users_mensas)))
 
-print("unique users %d" % len(set([i[0] for i in users_mensas])))
-print("unique mensas %d" % len(set([i[1] for i in users_mensas])))
+mensas = get_all_mensa_subscriptions()
+print("unique mensas %d" % len(mensas))
 print("list of mensas: ")
-print(set([i[1] for i in users_mensas]))
+print(mensas)
