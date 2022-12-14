a71b0a9c26af1b683d5402552b921666c29c5fbe
Frank Schüssele <fr.schuessele@gmx.de>
2022-12-14 22:26:11 +0100

    flake8 fix

--- Bot/config.py
+++ Bot/config.py
@@ -1,12 +1,13 @@
 import json
 
+
 class Config:
     def __init__(self, config_name):
         with open(config_name) as f:
             d = json.load(f)
         self.__admin_ids = d["admin_ids"]
         self.__token = d["token"]
-    
+
     def get_admin_ids(self):
         return self.__admin_ids
 
