cac492d919f25ac5a6666b4910ef780ce5d7844e
Frank Schüssele <fr.schuessele@gmx.de>
2022-12-17 19:34:01 +0100

    Add mensas and db_name to config (without using them yet)

--- Bot/config.py
+++ Bot/config.py
@@ -7,9 +7,17 @@ class Config:
             d = json.load(f)
         self.__admin_ids = d["admin_ids"]
         self.__token = d["token"]
+        self.__mensas = sorted(d["mensas"])
+        self.__db_name = d["db_name"]
 
     def get_admin_ids(self):
         return self.__admin_ids
 
     def get_token(self):
         return self.__token
+
+    def get_mensas(self):
+        return self.__mensas
+
+    def get_db_name(self):
+        return self.__db_name
