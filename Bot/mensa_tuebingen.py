a28d84b5281850911e910cff41069cfa1d61f050
Frank Schüssele <fr.schuessele@gmx.de>
2023-10-25 22:38:50 +0200

    Extract emojis

--- Bot/mensa_tuebingen.py
+++ Bot/mensa_tuebingen.py
@@ -1,6 +1,7 @@
 from urllib.request import urlopen
 import json
 from datetime import datetime
+import re
 
 
 MENSA_IDS = {'Mensa Shedhalle': '611', 'Mensa Nürtingen': '665',
@@ -8,6 +9,12 @@ MENSA_IDS = {'Mensa Shedhalle': '611', 'Mensa Nürtingen': '665',
              'Mensa Hohenheim': '661', 'Mensa Sigmaringen': '640',
              'Mensa Albstadt-Ebingen': '645', 'Mensa Rottenburg': '655',
              'Cafeterien MM/HSZ/San/TTR': '724', 'Mensa Prinz Karl': '623'}
+
+EMOJIS = {"F": "&#x1F41F", "G": "&#x1F414", "L": "&#x1F411", "R": "&#x1F404",
+          "K": "&#x1F404", "S": "&#x1F416", "V": "&#x1F331",
+          "vegan": "&#x1F331"}
+
+REGEX = re.compile(r'\s*\[(.{1,10})\]')
 
 
 def is_supported(mensa):
@@ -24,8 +31,13 @@ def retrieve_menus(mensa):
         date = format_date(m['menuDate'])
         if date not in res:
             res[date] = []
-        # TODO: Extract emojis
-        res[date].append((m['menuLine'], ', '.join(m['menu']), ""))
+        desc = ', '.join(m['menu'])
+        ing = {EMOJIS[i] for x in REGEX.findall(desc)
+               for i in x.split("/") if i in EMOJIS}
+        title = m['menuLine']
+        if "vegan" in desc:
+            title += " (vegan)"
+        res[date].append((title, REGEX.sub("", desc), list(ing)))
     return res
 
 
